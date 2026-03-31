a1fbd0ce679356240d4fa04a8b4fffd7d5479cbd
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-03-31 20:27:37 +0800

    basic app usage structure

--- letsplay/steam/main.py
+++ letsplay/steam/main.py
@@ -1,4 +1,7 @@
+from typing import Annotated
+
 import typer
+from typer import Argument, Option
 
 app = typer.Typer()
 
@@ -7,5 +10,18 @@ app = typer.Typer()
     no_args_is_help=True,
     help='Launch steam games',
 )
-def steam() -> None:
-    pass
+def steam(
+    app_id: Annotated[int, Argument(help='The Steam App ID of the game to launch')],
+    show_steam: Annotated[bool, Option('--show-steam', help='Show steam window')] = False,
+    mangohud: Annotated[bool, Option('--mangohud', help='Enable MangoHUD overlay')] = False,
+    launch_options: Annotated[str, Option('--launch-options', '-l', help='Additional Steam launch arguments')] = '',
+) -> None:
+    # command to use
+    cmd = ' '.join((part for part in (
+        'mangohud' if mangohud else '',
+        'steam',
+        '' if show_steam else '-silent',
+        f'-applaunch {app_id}',
+    ) if part))
+    print(f'{cmd=}')
+    print(f'{launch_options=}')
