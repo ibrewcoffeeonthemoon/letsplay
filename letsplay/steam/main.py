5d5e4baab5d677aab65e415cc97b781c1fdd471b
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2026-03-31 21:31:23 +0800

    comments

--- letsplay/steam/main.py
+++ letsplay/steam/main.py
@@ -14,9 +14,18 @@ def steam(
     app_id: Annotated[int, Argument(help='The Steam App ID of the game to launch')],
     show_steam: Annotated[bool, Option('--show-steam', help='Show steam window')] = False,
     mangohud: Annotated[Literal['env', 'cmd'] | None, Option('--mangohud', help='Enable MangoHUD overlay')] = None,
-    launch_options: Annotated[str, Option('--launch-options', '-l', help='Additional Steam launch arguments')] = '',
+    launch_options: Annotated[str | None, Option('--launch-options', '-l', help='Additional Steam launch arguments')] = None,
 ) -> None:
-    # command to use
+    # 1. kill all
+    # pkill -e -f "steam|gamescope|steamvr|wine"
+
+    # 2. launch_options
+    if launch_options:
+        # change launch options in steam config:
+        # ~/.local/share/Steam/userdata/1441367642/config/localconfig.vdf
+        pass
+
+    # 3. command to use
     cmd = ' '.join((part for part in (
         'mangohud' if mangohud == 'cmd' else 'MANGOHUD=1' if mangohud == 'env' else '',
         'steam',
@@ -25,3 +34,4 @@ def steam(
     ) if part))
     print(f'{cmd=}')
     print(f'{launch_options=}')
+    # execute this command
